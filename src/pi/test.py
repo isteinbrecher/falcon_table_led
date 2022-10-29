3cf1a9f61111dd205a00a8931469cca790cc91f7
Ivo Steinbrecher <stoani89@gmx.at>
2022-10-29 20:02:19 +0200

    Add rainbow

--- src/pi/test.py
+++ src/pi/test.py
@@ -41,7 +41,7 @@ print(checksum)
 # assert crc_calculator.verify_checksum(data, expected_checksum)
 
 
-rgb_color = bytes([20, 20, 200, 1, 255])
+rgb_color = bytes([20, 20, 200, 2, 100, 100])
 crc_calculator = CrcCalculator(Crc8.CCITT)
 checksum = crc_calculator.calculate_checksum(rgb_color)
 
--- src/table_controller/table_controller.ino
+++ src/table_controller/table_controller.ino
@@ -3,14 +3,14 @@
 // Setup data for the LED strip
 #include <NeoPixelBrightnessBus.h>
 constexpr size_t pin_strip = 3;
-constexpr size_t led_strip_num_pixels = 10;
+constexpr size_t led_strip_num_pixels = 192;
 using LedStrip = NeoPixelBrightnessBus<NeoGrbFeature, Neo800KbpsMethod>;
 LedStrip led_strip(led_strip_num_pixels, pin_strip);
 
 
 // Global data that is exchanged via IO
-constexpr size_t n_data = 5;
-uint8_t global_data[n_data] = {0, 0, 0, 0, 0};
+constexpr size_t n_data = 6;
+uint8_t global_data[n_data] = {0, 0, 0, 0, 0, 0};
 bool global_data_updated = true;
 
 // This include has to be defined after the data variables are initialized
@@ -22,6 +22,9 @@ inline uint8_t get_global_mode() { return global_data[3]; }
 
 // Get the overall brightness
 inline uint8_t get_global_brightness() { return global_data[4]; }
+
+// Get the global delay
+inline uint8_t get_global_delay() { return global_data[5]; }
 
 // Get the global color value
 inline RgbColor get_global_color() { return {global_data[0], global_data[1], global_data[2]}; }
@@ -46,7 +49,7 @@ void strip_off()
     {
       if (my_mode != get_global_mode()) return;
 
-      for (size_t i = 0; i < led_strip.PixelCount(); i++)
+      for (size_t i = 0; i < led_strip_num_pixels; i++)
       {
         led_strip.SetPixelColor(i, RgbColor{0, 0, 0});
       }
@@ -72,7 +75,7 @@ void constant_color_mode()
 
       // The strip has to be redrawn, also set the brightness since that could have also changed
       led_strip.SetBrightness(get_global_brightness());
-      for (size_t i = 0; i < led_strip.PixelCount(); i++)
+      for (size_t i = 0; i < led_strip_num_pixels; i++)
       {
         led_strip.SetPixelColor(i, get_global_color());
       }
@@ -84,6 +87,55 @@ void constant_color_mode()
     delay(default_delay);
   }
 }
+
+/**
+ * \brief Display a moving rainbow
+ */
+void rainbow_cycle()
+{
+  const uint8_t my_mode = get_global_mode();
+
+  while (true)
+  {
+    for (uint8_t j = 0; j < 256; j++)
+    {
+      if (my_mode != get_global_mode()) return;
+
+      rainbow(j);
+      delay(get_global_delay());
+    }
+  }
+}
+
+/**
+ * \brief Display a rainbow on the strip
+ */
+void rainbow(const uint8_t j)
+{
+  for (uint16_t i = 0; i < led_strip_num_pixels; i++)
+  {
+    led_strip.SetPixelColor(i, Wheel(((i * 256 / led_strip_num_pixels) + j) & 255));
+  }
+  led_strip.Show();
+}
+
+// (from adafruit library)
+RgbColor Wheel(uint8_t i_pos)
+{
+  i_pos = 255 - i_pos;
+  if (i_pos < 85)
+  {
+    return RgbColor(255 - i_pos * 3, 0, i_pos * 3);
+  }
+  if (i_pos < 170)
+  {
+    i_pos -= 85;
+    return RgbColor(0, i_pos * 3, 255 - i_pos * 3);
+  }
+  i_pos -= 170;
+  return RgbColor(i_pos * 3, 255 - i_pos * 3, 0);
+}
+
 
 /**
  * \brief Setup and initialise variables
@@ -118,6 +170,11 @@ void loop()
     case 1:
     {
       constant_color_mode();
+      break;
+    }
+    case 2:
+    {
+      rainbow_cycle();
       break;
     }
   }
