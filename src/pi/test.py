bfe010c12dc6d9f1f853774813060c20ef64b32c
Ivo Steinbrecher <stoani89@gmx.at>
2022-10-29 18:10:35 +0200

    Working to set the color

--- src/pi/test.py
+++ src/pi/test.py
@@ -1,6 +1,5 @@
 from crc import CrcCalculator, Crc8
 
-
 data = bytes([1, 23, 8, 254, 0, 124])
 crc_calculator = CrcCalculator(Crc8.CCITT)
 
@@ -8,3 +7,15 @@ checksum = crc_calculator.calculate_checksum(data)
 print(checksum)
 # assert checksum == expected_checksum
 # assert crc_calculator.verify_checksum(data, expected_checksum)
+
+
+rgb_color = bytes([0, 40, 60])
+crc_calculator = CrcCalculator(Crc8.CCITT)
+checksum = crc_calculator.calculate_checksum(rgb_color)
+
+import smbus2
+
+I2C_SLAVE_ADDRESS = 11
+
+with smbus2.SMBus(1) as I2Cbus:
+    I2Cbus.write_i2c_block_data(I2C_SLAVE_ADDRESS, checksum, rgb_color)
--- src/table_controller/table_controller.ino
+++ src/table_controller/table_controller.ino
@@ -2,36 +2,41 @@
 #include <NeoPixelBrightnessBus.h>
 
 
+// For i2c
+#include <Wire.h>
+
+
+#define I2C_SLAVE_ADDRESS 11
+
+
 const unsigned int pin_strip = 3;
-const unsigned int n_buttons = 3;
-const unsigned int pin_button_bottom = 12;
-const unsigned int pin_button_middle = 9;
-const unsigned int pin_button_top = 2;
 
 // led strip constants
 const unsigned int led_strip_num_pixels = 10;
 
 
-using LedStrip = NeoPixelBrightnessBus<NeoRgbFeature, Neo800KbpsMethod>;
+using LedStrip = NeoPixelBrightnessBus<NeoGrbFeature, Neo800KbpsMethod>;
 LedStrip led_strip(led_strip_num_pixels, pin_strip);
+
+
+CRC8 crc;
+
+uint32_t color[3] = {0, 10, 0};
 
 void setup()
 {
+  Wire.begin(I2C_SLAVE_ADDRESS);
+  Wire.onReceive(receiveEvents);
+
   Serial.begin(9600);
 
   // start the led strip
   led_strip.Begin();
   led_strip.Show();
 
-  for (unsigned int i = 0; i < led_strip_num_pixels; i++)
-  {
-    led_strip.SetPixelColor(i, RgbColor(10, 10, 10));
-  }
-  led_strip.Show();
 
 
   // Perform a CRC check
-  CRC8 crc;
   crc.add(1);
   crc.add(23);
   crc.add(8);
@@ -42,4 +47,57 @@ void setup()
   Serial.print(crc.getCRC());
 }
 
-void loop() {}
+void loop()
+{
+  for (unsigned int i = 0; i < led_strip_num_pixels; i++)
+  {
+    led_strip.SetPixelColor(i, RgbColor(color[0], color[1], color[2]));
+  }
+  led_strip.Show();
+
+  delay(100);
+}
+
+void receiveEvents(int n_bytes)
+{
+  if (n_bytes != 4)
+  {
+    // We expect exactly 4 bytes, everything else indicates an error, i.e., do nothing and empty the
+    // read stack
+    while (Wire.available())
+    {
+      Wire.read();
+    }
+  }
+  else
+  {
+    // The first byte is the checksum
+    const uint32_t checksum = Wire.read();
+
+    // Get the color values
+    uint32_t r = Wire.read();
+    uint32_t g = Wire.read();
+    uint32_t b = Wire.read();
+
+    // Check the checksum
+    CRC8 crc;
+    crc.add(r);
+    crc.add(g);
+    crc.add(b);
+    const bool is_crc_ok = checksum == crc.getCRC();
+
+    if (is_crc_ok)
+    {
+      color[0] = r;
+      color[1] = g;
+      color[2] = b;
+    }
+
+    if (is_crc_ok)
+      Serial.print("Checksum ok");
+    else
+      Serial.print("Checksum WRONG");
+
+    if (Wire.available()) Serial.print("Error, more bytes available");
+  }
+}
